717cc14ace498d7ace99c3968937a0e953e5a43c
Elizabeth Sham <esham00@stuy.edu>
2019-03-12 01:13:39 -0400

    works but takes a really long time

--- Quick.java
+++ Quick.java
@@ -3,12 +3,12 @@ public class Quick {
     public static int partition(int[] data, int start, int end) {
 	Random rand = new Random();
 	int a = Math.abs(rand.nextInt()%data.length);
-	System.out.println(data[a] + " Random Index:" + a);
+	//System.out.println(data[a] + " Random Index:" + a);
 	int old = data[0];
 	data[0] = data[a];
 	data[a] = old;
 	while(start < end) {
-	    System.out.println("old start, old end: " + start + ", " + end);
+	    //System.out.println("old start, old end: " + start + ", " + end);
 	    if (data[start] < data[0]) {
 		start++;
 	    } else {
@@ -17,7 +17,7 @@ public class Quick {
 		data[end] = old;
 		end--;
 	    }
-	    System.out.println("new start, new end: " + start + ", " + end);
+	    //System.out.println("new start, new end: " + start + ", " + end);
 	}
 	if (data[start] > data[0]) {
 	    old = data[start-1];
@@ -38,9 +38,29 @@ public class Quick {
 	}
 	return output;
     }
+    public static int quickSelect(int[] data, int k) {
+	int start = 1;
+	int end = data.length-1;
+	int value = partition(data, start, end);
+	while (value != k) {
+	    if (value < k) {
+		end = end/2;
+	    } else {
+		start = end/2;
+	    }
+	    value = partition(data, start, end);
+	}
+	return data[value];
+    }
     public static void main(String[] args) {
-	int[] data = new int[] {999,4,1,0,3,2};
-	System.out.println(partition(data, 1, data.length-1));
+	int[] data = new int[] {4,3,2,1,6,5,7};
+	// System.out.println(partition(data, 1, data.length-1));
+	// System.out.println(toString(data));
+	System.out.println(quickSelect(data, 3));
+	System.out.println(toString(data));
+	System.out.println(quickSelect(data, 4));
+	System.out.println(quickSelect(data, 5));
+	System.out.println(quickSelect(data, 2));
 	System.out.println(toString(data));
     }
 }
