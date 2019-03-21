a9ece197b5fd6e5007c3064df28dbe45d201f409
Elizabeth Sham <esham00@stuy.edu>
2019-03-21 02:30:18 -0400

    added insertion sort

--- Quick.java
+++ Quick.java
@@ -11,6 +11,10 @@ public class Quick {
 	int index = start;
 	data[start] = data[a];
 	data[a] = old;
+	if (end - start < 10) {
+	    insertionsort(data, start, end, data[a], start);
+	    return start;
+	}
 	//adding 1 to the start so it would go onto the next value to compare
 	if (end != start) {
 	    start++;
@@ -99,8 +103,74 @@ public class Quick {
 	}
     }
     public static void quicksort(int[] data) {
+	if (data.length == 0) {
+	    return;
+	}
 	quicksortH(data, 0, data.length-1);
     }
+    // public static void quicksortDutch(int[] data) {
+    // 	quicksortDutchH(data, 0, data.length-1);
+    // }
+    // public static void quicksortDutchH(int[] data, int start, int end) {
+    // 	if (start >= end) {
+    // 	    return;
+    // 	} else {
+    // 	    int[] pivot = partitionDutch(data, start, end);
+    // 	    System.out.println("lt: " + pivot[0] + " gt: " + pivot[1]);
+    // 	    quicksortDutchH(data, start, pivot[0]-1);
+    // 	    quicksortDutchH(data, pivot[1]+1, end);
+    // 	}
+    // }
+    // public static int[] partitionDutch(int[] data, int start, int end) {
+    // 	int lt = 0;
+    // 	int gt = 0;
+    // 	//random
+    // 	Random rand = new Random();
+    // 	//generating random index
+    // 	int a = Math.abs(rand.nextInt()%(end - start+1))+ start;
+    // 	//swapped chosen values
+    // 	int old = data[start];
+    // 	int index = start;
+    // 	data[start] = data[a];
+    // 	data[a] = old;
+    // 	//adding 1 to the start so it would go onto the next value to compare
+    // 	if (end != start) {
+    // 	    start++;
+    // 	}
+    // 	while(start < end) {
+    // 	    //compare if chosen value is more than the comparison value move onto the next value
+    // 	    if (data[start] <  data[index]) {
+    // 		start++;
+    // 	    } else if (data[start] == data[index]) {
+    // 		if (lt < start) {
+    // 		    lt = start;
+    // 		}
+    // 		start++;
+    // 	    } else {
+    // 		//otherwise swap places at the end
+    // 		old = data[start];
+    // 		data[start] = data[end];
+    // 		data[end] = old;
+    // 		end--;
+    // 	    }
+    // 	}
+    // 		//if the index at start is larger than the chosen value then swap indices with the index 1 before the start
+    // 	if (data[start] > data[index]) {
+    // 	    old = data[start-1];
+    // 	    data[start-1] = data[index];
+    // 	    data[index] = old;
+    // 	    gt = start-1;
+    // 	    return new int[]{lt,gt};
+    // 	} else {
+    // 	    //otherwise swap indices 
+    // 	    old = data[start];
+    // 	    data[start] = data[index];
+    // 	    data[index] = old;
+    // 	    gt = start;
+    // 	    return new int[] {lt, gt};
+    // 	}
+    // }
+
      public static void main(String[] args) {
        int[] data = new int[] {4,6,3,8, 2, 999, 1,6,999,5,7};
     // 	// System.out.println(partition(data, 1, data.length-1));
@@ -111,8 +181,6 @@ public class Quick {
     // 	//System.out.println(quickSelect(data, 5));
     //     //System.out.println(quickSelect(data, 2));
     //     //System.out.println(toString(data));
-    	quicksortH(data, 0, data.length-1);
-    	System.out.println(Arrays.toString(data));
     	System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
     	int[]MAX_LIST = {1000000000,500,10};
     	for(int MAX : MAX_LIST){
@@ -146,5 +214,105 @@ public class Quick {
     	    System.out.println();
     	}
     }
+    private static final int INCREASE = 0;
+    private static final int DECREASE = 1;
+    private static final int STANDARD = 2;
+    private static final int SMALL_RANGE = 3;
 
+    private static String name(int i){
+	if(i==INCREASE)return "Increassing";
+	if(i==DECREASE)return "Decreassing";
+	if(i==STANDARD)return "Normal Random";
+	if(i==SMALL_RANGE)return "Random with Few Values";
+	return "Error categorizing array";
+
+    }
+
+    private static int create(int min, int max){
+	return min + (int)(Math.random()*(max-min));
+    }
+
+    private static int[]makeArray(int size,int type){
+	int[]ans =new int[size];
+	if(type == STANDARD){
+	    for(int i = 0; i < size; i++){
+		ans[i]= create(-1000000,1000000);
+	    }
+	}
+	else if(type == INCREASE){
+	    int current = -5 * size;
+	    for(int i = 0; i < size; i++){
+		ans[i]= create(current,current + 10);
+		current += 10;
+	    }
+	}
+	else if(type == DECREASE){
+	    int current = 5 * size;
+	    for(int i = 0; i < size; i++){
+		ans[i]= create(current,current + 10);
+		current -= 10;
+	    }
+	}
+	else if(type == SMALL_RANGE){
+	    for(int i = 0; i < size; i++){
+		ans[i]= create(-5,5);
+	    }
+	}
+	else{
+	    ans = new int[0];//empty is default
+	}
+	return ans;
+    }
+    public static void insertionsort(int[] data, int lo, int hi, int old, int index) {
+	for(int i = lo+1; i < hi+1; i++) {
+	    int current = data[i];
+	    int j = i-1;
+	    while(j >= lo && current < data[j]) {
+		if (current == old) {
+		    index = j;
+		}
+		data[j+1] = data[j];
+		j--;
+	    }
+	    if (current == old) {
+		index = j+1;
+	    }
+	    data[j+1] = current;
+	}
+    }
+    public static void insertionsort(int[] data) {
+	for(int i = 1; i < data.length; i++) {
+	    int current = data[i];
+	    int j = i-1;
+	    while(j >= 0 && current < data[j]) {
+		data[j+1] = data[j];
+		j--;
+	    }
+	    data[j+1] = current;
+	}
+    }
+	
+
+    // public static void main(String[]args){
+    // 	if(args.length < 2)return;
+    
+    // 	int size =  Integer.parseInt(args[0]);
+    // 	int type =   Integer.parseInt(args[1]);
+
+    // 	int [] start = makeArray(size,type);
+    // 	int [] result = Arrays.copyOf(start,start.length);
+    // 	Arrays.sort(result);
+    
+    // 	long startTime = System.currentTimeMillis();
+    // 	/*
+    // 	 * Test your sort here //yoursort(start);
+    // 	 * Add code to switch which sort is tested by changing one of the args!
+    // 	 */
+    // 	long elapsedTime = System.currentTimeMillis() - startTime;
+    // 	if(Arrays.equals(start,result)){
+    // 	    System.out.println("PASS Case "+name(type)+"\t array, size:"+start.length+"\t"+elapsedTime/1000.0+"sec ");
+    // 	}else{
+    // 	    System.out.println("FAIL ! ERROR ! "+name(type)+" array, size:"+size+"  ERROR!");
+    // 	}
+    // }
 }
